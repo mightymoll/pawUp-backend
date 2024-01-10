04c606871ae714b2b7809ddc14a112093bb72aca
molly <mnfelts@gmail.com>
2024-01-10 09:02:02 +0100

    add express.json back

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /** IMPORTS **/
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
